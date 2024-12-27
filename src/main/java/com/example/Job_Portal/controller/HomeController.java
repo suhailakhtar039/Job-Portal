ceb16dce637c3847697d688e8fab86225ea5edcb
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-27 11:32:25 +0530

    home controller get mapping

--- src/main/java/com/example/Job_Portal/controller/HomeController.java
+++ src/main/java/com/example/Job_Portal/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.example.Job_Portal.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
 }
