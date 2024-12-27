bf52715b19ebf80229c22757b2ad67770b312a9f
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-27 17:40:03 +0530

    implementing get all

--- src/main/java/com/example/Job_Portal/services/UsersTypeService.java
+++ src/main/java/com/example/Job_Portal/services/UsersTypeService.java
@@ -1,7 +1,10 @@
 package com.example.Job_Portal.services;
 
+import com.example.Job_Portal.entity.UsersType;
 import com.example.Job_Portal.repository.UsersTypeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsersTypeService {
@@ -10,6 +13,10 @@ public class UsersTypeService {
 
     public UsersTypeService(UsersTypeRepository usersTypeRepository) {
         this.usersTypeRepository = usersTypeRepository;
+    }
+
+    public List<UsersType> getAll(){
+        return usersTypeRepository.findAll();
     }
 
 }
