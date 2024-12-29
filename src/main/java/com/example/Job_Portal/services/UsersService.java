09c150df1f6ce05cf12c23ce19708e37ba2abe52
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-29 16:39:49 +0530

    repository injected using constructor

--- src/main/java/com/example/Job_Portal/services/UsersService.java
+++ src/main/java/com/example/Job_Portal/services/UsersService.java
@@ -1,7 +1,19 @@
 package com.example.Job_Portal.services;
 
+import com.example.Job_Portal.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+
+
 }
