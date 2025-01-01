92cff7267e4c3d1afa6154459001ca910af1e880
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-01 15:48:10 +0530

    jobseeker file entity created

--- src/main/java/com/example/Job_Portal/entity/JobSeekerProfile.java
+++ src/main/java/com/example/Job_Portal/entity/JobSeekerProfile.java
@@ -1,4 +1,33 @@
 package com.example.Job_Portal.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "job_seeker_profile")
 public class JobSeekerProfile {
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users users;
+
+    private String city;
+
+    private String country;
+
+    private String employmentType;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePhoto;
+
+    private String resume;
+
+    private String state;
+
+    private String workAuthorization;
 }
