ff4e0d22dda3b0a780f218d969e337d85cbb708e
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-01 15:14:40 +0530

    user service invoked to check for duplicate email

--- src/main/java/com/example/Job_Portal/controller/UsersController.java
+++ src/main/java/com/example/Job_Portal/controller/UsersController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
@@ -33,7 +34,15 @@ public class UsersController {
     }
 
     @PostMapping("/register/new")
-    public String userRegistration(@Valid Users users){
+    public String userRegistration(@Valid Users users, Model model){
+        Optional<Users> userByEmail = usersService.getUserByEmail(users.getEmail());
+        if(userByEmail.isPresent()){
+            model.addAttribute("error", "Email already registered, try to login or register with other email.");
+            List<UsersType> usersTypes = usersTypeService.getAll();
+            model.addAttribute("getAllTypes", usersTypes);
+            model.addAttribute("user", new Users());
+            return "register";
+        }
         usersService.addNew(users);
         return "dashboard";
     }
