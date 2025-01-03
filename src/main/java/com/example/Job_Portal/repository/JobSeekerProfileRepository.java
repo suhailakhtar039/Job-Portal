e5b31ee3420e2713d351bacd44ae2471bfc14bd8
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-03 16:52:10 +0530

    job seeker profile repository created

---
+++ src/main/java/com/example/Job_Portal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.example.Job_Portal.repository;
+
+import com.example.Job_Portal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+}
