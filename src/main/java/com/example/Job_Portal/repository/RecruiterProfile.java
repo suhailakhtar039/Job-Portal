825dc380742c884c97e313e930d478059ed56ee4
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-03 16:50:36 +0530

    recruiter profile created

--- src/main/java/com/example/Job_Portal/repository/RecruiterProfile.java
+++
@@ -1,4 +0,0 @@
-package com.example.Job_Portal.repository;
-
-public interface RecruiterProfile {
-}
---
+++ src/main/java/com/example/Job_Portal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.example.Job_Portal.repository;
+
+import com.example.Job_Portal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+}
