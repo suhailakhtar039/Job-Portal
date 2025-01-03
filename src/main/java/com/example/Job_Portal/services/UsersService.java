48e7f2b36e9bee75d7f83983f382d53e9bb6bb23
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-03 16:56:51 +0530

    job repo and recruiter repo injected

--- src/main/java/com/example/Job_Portal/services/UsersService.java
+++ src/main/java/com/example/Job_Portal/services/UsersService.java
@@ -1,6 +1,9 @@
 package com.example.Job_Portal.services;
 
+import com.example.Job_Portal.entity.JobSeekerProfile;
 import com.example.Job_Portal.entity.Users;
+import com.example.Job_Portal.repository.JobSeekerProfileRepository;
+import com.example.Job_Portal.repository.RecruiterProfileRepository;
 import com.example.Job_Portal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,15 +15,20 @@ import java.util.Optional;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
         this.usersRepository = usersRepository;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
     }
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        int userTypeId = users.getUserTypeId().getUserTypeId();
         return usersRepository.save(users);
     }
 
