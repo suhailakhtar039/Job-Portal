ae8ec39cbf1fb201ee503653d7557317c7222da6
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-25 13:32:46 +0530

    username not found

--- src/main/java/com/example/Job_Portal/services/CustomUserDetailsService.java
+++ src/main/java/com/example/Job_Portal/services/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.example.Job_Portal.services;
 
 import com.example.Job_Portal.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +10,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UsersRepository usersRepository;
 
+    @Autowired
     public CustomUserDetailsService(UsersRepository usersRepository) {
         this.usersRepository = usersRepository;
     }
@@ -16,6 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        usersRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("Could not find user"));
         return null;
     }
 }
