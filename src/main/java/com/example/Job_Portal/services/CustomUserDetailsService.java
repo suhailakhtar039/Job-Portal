4fc6ed35294a07a26bb1d7c742505b7e6d0d3bdc
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-25 13:01:38 +0530

    user repository injected

--- src/main/java/com/example/Job_Portal/services/CustomUserDetailsService.java
+++ src/main/java/com/example/Job_Portal/services/CustomUserDetailsService.java
@@ -1,4 +1,21 @@
 package com.example.Job_Portal.services;
 
-public class CustomUserDetailsService {
+import com.example.Job_Portal.repository.UsersRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
