120f6b9e6f74d41e6430113dbcb77107e7855bd3
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-01-25 12:57:26 +0530

    authentication

--- src/main/java/com/example/Job_Portal/config/WebSecurityConfig.java
+++ src/main/java/com/example/Job_Portal/config/WebSecurityConfig.java
@@ -2,7 +2,11 @@ package com.example.Job_Portal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,11 +29,26 @@ public class WebSecurityConfig {
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authenticationProvider(authenticationProvider());
+
         http.authorizeHttpRequests(auth->{
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
         return http.build();
+    }
+
+    @Bean
+    private AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService();
+    }
+
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
