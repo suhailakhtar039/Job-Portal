3f2c4db9a6021cea68768c9da2f340a181e7307b
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-02-03 15:11:04 +0530

    customer user details changed

--- src/main/java/com/example/Job_Portal/util/CustomUserDetails.java
+++ src/main/java/com/example/Job_Portal/util/CustomUserDetails.java
@@ -1,5 +1,53 @@
 package com.example.Job_Portal.util;
 
-public class CustomUserDetails {
+import com.example.Job_Portal.entity.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Users users;
+
+    @Autowired
+    public CustomUserDetails(Users users) {
+        this.users = users;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
